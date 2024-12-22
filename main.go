6a43f80e6386784f953e5b276df66d84e1e4f9e7
Max-Gabriel-Susman <gabe.susman@gmail.com>
2024-12-22 00:38:15 -0700

    included inference logic

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main 
 
 func main() {
+	wg := sync.WaitGroup{}
+
+	tf := tensorflow.NewTensorflowMachineLearning()
+	openCV := opencv.NewOpenCVImageProcessor()
+
+	modelPath, err := filepath.Abs("data/models/ssd_mobilenet_v1_coco_2018_01_28/saved_model")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	model, tfSession, err := tf.LoadSavedModel(modelPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer model.Session.Close()
+	defer tfSession.Close()
+
+	runDetectionUseCase := usecase.NewRunDetectionUseCase(tf, openCV)
+	 
 	log.Println("Argus Stream Engine Service online.")
+
+	for index, camera := range getCameras() {
+		wg.Add(1)
+		defer wg.Done()
+
+		go worker(index+1, camera, openCV, runDetectionUseCase)
+	}
+
+	wg.Wait()
 }
