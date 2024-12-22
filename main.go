95465ccfbcf45b1d8fd14ca292d2dbd541046d5f
Max-Gabriel-Susman <gabe.susman@gmail.com>
2024-12-22 00:51:14 -0700

    included std library logic

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
-package main 
+package main
+
+import (
+	"log"
+	"path/filepath"
+	"sync"
+)
 
 func main() {
 	wg := sync.WaitGroup{}
 
-	tf := tensorflow.NewTensorflowMachineLearning()
-	openCV := opencv.NewOpenCVImageProcessor()
+	// tf := tensorflow.NewTensorflowMachineLearning()
+	// openCV := opencv.NewOpenCVImageProcessor()
 
-	modelPath, err := filepath.Abs("data/models/ssd_mobilenet_v1_coco_2018_01_28/saved_model")
+	// modelPath, err := filepath.Abs("data/models/ssd_mobilenet_v1_coco_2018_01_28/saved_model")
+	_, err := filepath.Abs("data/models/ssd_mobilenet_v1_coco_2018_01_28/saved_model")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	model, tfSession, err := tf.LoadSavedModel(modelPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer model.Session.Close()
-	defer tfSession.Close()
+	// model, tfSession, err := tf.LoadSavedModel(modelPath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer model.Session.Close()
+	// defer tfSession.Close()
 
-	runDetectionUseCase := usecase.NewRunDetectionUseCase(tf, openCV)
-	 
+	// runDetectionUseCase := usecase.NewRunDetectionUseCase(tf, openCV)
+
 	log.Println("Argus Stream Engine Service online.")
 
-	for index, camera := range getCameras() {
-		wg.Add(1)
-		defer wg.Done()
+	// for index, camera := range getCameras() {
+	// 	wg.Add(1)
+	// 	defer wg.Done()
 
-		go worker(index+1, camera, openCV, runDetectionUseCase)
-	}
+	// 	go worker(index+1, camera, openCV, runDetectionUseCase)
+	// }
 
 	wg.Wait()
 }
